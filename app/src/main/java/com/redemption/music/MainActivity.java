657552657f2bd21957b78a98b12ed1b43c6ef3f5
Engineer_mikiyas <ewenetmikiyas@gmail.com>
2022-09-14 11:19:10 +0300

    search view done only edit text part

--- app/src/main/java/com/redemption/music/MainActivity.java
+++ app/src/main/java/com/redemption/music/MainActivity.java
@@ -3,16 +3,18 @@ package com.redemption.music;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -20,44 +22,30 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
+import com.redemption.music.Models.Music;
 
 import java.io.File;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import ir.mirrajabi.searchdialog.SimpleSearchDialogCompat;
+import ir.mirrajabi.searchdialog.core.BaseSearchDialogCompat;
+import ir.mirrajabi.searchdialog.core.SearchResultListener;
 
-//    ListView listView;
-//    String[] items;
+public class MainActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
-
-    ListView listView;
-    String[] name = {
-            "test", "nata", "amazing"
-    };
-    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        listView = findViewById(R.id.songsList);
-//
-//        runTimePermission();
-
         // drawer layout instance to toggle the menu icon to open
         // drawer and back button to close drawer
         drawerLayout = findViewById(R.id.my_drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        listView = findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
-        listView.setAdapter(arrayAdapter);
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
@@ -81,73 +69,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search, menu);
-        MenuItem menuItem = menu.findItem(R.id.actionSearch);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Type here to Search");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        MenuItem.OnActionExpandListener onActionExpandListener = new MenuItem.OnActionExpandListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, "Menu Expanded", Toast.LENGTH_LONG).show();
+                return true;
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
-                return false;
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, "Menu Collapsed", Toast.LENGTH_LONG).show();
+                return true;
             }
-        });
-        return super.onCreateOptionsMenu(menu);
+        };
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search ...");
+        return true;
     }
-
-    //    public void runTimePermission() {
-//        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
-//            @Override
-//            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-//                displaySongs();
-//            }
-//
-//            @Override
-//            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-//
-//            }
-//
-//            @Override
-//            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-//                permissionToken.continuePermissionRequest();
-//            }
-//        }).check();
-//    }
-
-//    public ArrayList<File> findSong (File file) {
-//        ArrayList<File> arrayList = new ArrayList<>();
-//
-//        File[] files = file.listFiles();
-//
-//
-//
-//        for (File singleFile: files) {
-//            if (singleFile.isDirectory() && !singleFile.isHidden()) {
-//                arrayList.addAll(findSong(singleFile));
-//            } else {
-//                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
-//                    arrayList.add(singleFile);
-//                }
-//            }
-//        }
-//        return arrayList;
-//    }
-
-//    void displaySongs () {
-//        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
-//
-//        items = new String[mySongs.size()];
-//
-//        for (int i = 0; i < mySongs.size(); i++) {
-//            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
-//        }
-//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-//        listView.setAdapter(myAdapter);
-//    }
 }
